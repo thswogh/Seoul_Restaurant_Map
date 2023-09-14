3f7b48d4cbaffe92124949afc7cd905482077abe
chiwon <dh5277@naver.com>
2023-09-14 11:35:57 +0900

    spring first commit

---
+++ Seoul_Restaurant_Map/src/main/java/Sejong/Seoul_Restaurant_Map/SeoulRestaurantMapApplication.java
@@ -0,0 +1,13 @@
+package Sejong.Seoul_Restaurant_Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulRestaurantMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulRestaurantMapApplication.class, args);
+	}
+
+}
---
+++ Seoul_Restaurant_Map/src/test/java/Sejong/Seoul_Restaurant_Map/SeoulRestaurantMapApplicationTests.java
@@ -0,0 +1,13 @@
+package Sejong.Seoul_Restaurant_Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulRestaurantMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
