a03e1ab4112c7ec5b4abd047d7dd44b21984236c
thwogh <daymos999@gmail.com>
2023-11-27 16:00:26 +0900

    [Feat] 메인페이지 지역 바꾸면 해당 위치로 지도 이동

--- front-end/src/component/mainComponent/Region.js
+++ front-end/src/component/mainComponent/Region.js
@@ -4,9 +4,27 @@ import { useMarkers } from "../util/MyContext";
 
 const SelectBox = (props) => {
     const { markers, setMarkers, mapInfo, setMapInfo } = useMarkers();
+    const handleSelectChange = (event) => {
+        const selectedValue = event.target.value;
+
+        // 선택된 옵션의 데이터 찾기
+        const selectedOption = props.options.find(option => option.value === selectedValue);
+        // selectedOption에서 lat 및 lng 변수에 접근
+        if (selectedOption) {
+            const { lat, lng, level } = selectedOption;
+            setMapInfo({
+                lat: lat,
+                lng: lng,
+                level: level,
+            });
+        }
+    }
 
     return (
-        <select style={{ marginBottom: "5vh", fontSize: "1rem" }}>
+        <select
+            style={{ marginBottom: "5vh", fontSize: "1rem" }}
+            onChange={handleSelectChange}
+        >
             {props.options.map((option) => (
                 <option
                     key={option.value}  // Added a key for each option to avoid a React warning
--- front-end/src/data/RegionData.js
+++ front-end/src/data/RegionData.js
@@ -1,8 +1,8 @@
 export const RegionData = [
-    { value: "서울", name: "서울" },
-    { value: "경기도", name: "경기도" },
-    { value: "대구", name: "대구" },
-    { value: "부산", name: "부산" },
-    { value: "제주도", name: "제주도" },
-    { value: "강원도", name: "강원도" },
+    { value: "서울", name: "서울", lat: 37.56100278, lng: 126.9996417, level: 8 },
+    { value: "경기도", name: "경기도", lat: 37.65590833, lng: 126.7770556, level: 8 },
+    { value: "대구", name: "대구", lat: 35.86952722, lng: 128.6061745, level: 9 },
+    { value: "부산", name: "부산", lat: 35.10321667, lng: 129.0345083, level: 9 },
+    { value: "제주도", name: "제주도", lat: 33.374278, lng: 126.537814, level: 10 },
+    { value: "인천", name: "인천", lat: 37.47384843, lng: 126.6217617, level: 8 },
 ];
