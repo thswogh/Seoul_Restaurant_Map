156f69a09ce45518548ef81dc8932b412423ea79
thwogh <daymos999@gmail.com>
2023-12-07 16:32:40 +0900

    deploy

--- front-end/src/component/map/Map.js
+++ front-end/src/component/map/Map.js
@@ -30,7 +30,7 @@ const BasicMap = () => {
         let y_end = neLatLng.getLat();
 
         try {
-            const response = await axios.get('/home/search', {
+            const response = await axios.get('https://35.216.106.118:8443/home/search', {
                 params: {
                     x_start: x_start,
                     x_end: x_end,
