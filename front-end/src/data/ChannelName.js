de094169463341b3e6e3edb30ab3047a8454ff93
thwogh <daymos999@gmail.com>
2023-12-13 22:03:31 +0900

    ChannelName 변경

--- front-end/src/data/ChannelName.js
+++ front-end/src/data/ChannelName.js
@@ -1,6 +1,5 @@
 export const ChannelNames = [
     '성시경 SUNG SI KYUNG',
-    '쏘야미',
     '떡볶퀸 Tteokbokqueen',
     '지뉼랭가이드',
     '정육왕 MeatCreator',
@@ -11,6 +10,5 @@ export const ChannelNames = [
     '잡식공룡',
     '섬마을훈태TV',
     '맛있겠다 Yummy',
-    '먹보스쭈엽이',
     '먹갱_Mukgang'
 ];
